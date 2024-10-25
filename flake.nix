f2377a45ce0335a6ca49bca50d7107b24f6935af
John Chadwick <john@jchw.io>
2024-10-25 11:43:31 -0400

    Add CI

--- flake.nix
+++ flake.nix
@@ -16,17 +16,62 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        zanbato = {
-          default = pkgs.buildGoModule {
-            name = "zanbato";
-            src = self;
-            vendorHash = pkgs.lib.fakeHash;
-          };
+        zanbato = pkgs.buildGoModule {
+          name = "zanbato";
+          src = self;
+          vendorHash = "sha256-jsz/yIWjo71TqXcz4Jfv6CE33eL+IHmC0rwgnqJyt0E=";
+        };
+        format = pkgs.writeShellApplication {
+          name = "format";
+
+          runtimeInputs = [
+            pkgs.nixfmt-rfc-style
+            pkgs.yamlfmt
+            pkgs.go
+          ];
+
+          text = ''
+            if [[ $# -ne 1 || "$1" == "--help" ]]; then
+              >&2 echo "Usage: $0 --check | --write"
+              exit 0
+            fi
+
+            NIXFMT_ARGS=()
+            YAMLFMT_ARGS=()
+
+            case $1 in
+              -w|--write)
+                NIXFMT_ARGS+=("--verify")
+                GOFMT_COMMAND="gofmt -w ."
+                shift
+                ;;
+              -c|--check)
+                NIXFMT_ARGS+=("--check")
+                YAMLFMT_ARGS+=("-dry" "-lint")
+                GOFMT_COMMAND="diff <(echo -n) <(gofmt -d .)"
+                shift
+                ;;
+              *)
+                >&2 echo "Unknown option $1"
+                exit 1
+                ;;
+            esac
+
+            >&2 echo "Running nixfmt."
+            find . -not -path '*/.*' -not -path 'build' -iname '*.nix' -print0 | \
+              xargs -0 nixfmt "''${NIXFMT_ARGS[@]}"
+
+            >&2 echo "Running yamlfmt."
+            yamlfmt "''${YAMLFMT_ARGS[@]}" '**.yml' .clang-format .clang-tidy
+
+            >&2 echo "Running gofmt."
+            bash -c "''${GOFMT_COMMAND}"
+          '';
         };
       in
       {
         packages = {
-          inherit zanbato;
+          inherit zanbato format;
           default = zanbato;
         };
         devShell = pkgs.mkShell {
